4236d00d6427e3803944e6bdbe76d6c01889fe73
Engels Prado <engelsprado7@gmail.com>
2024-09-20 22:56:33 -0600

    chore: Update package dependencies and add Astro Vercel adapter

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:3000';
+const URL = 'https://barber-shop-kf19.onrender.com'
 
 export const socket = io(URL);
