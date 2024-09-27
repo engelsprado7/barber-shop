ff8974a3b42251a742583fbab2968e9a42de346e
Engels Prado Herrera <99674354+engelsprado7@users.noreply.github.com>
2024-09-27 13:46:03 -0600

    Update twilioClient.js

--- server/twilioClient.js
+++ server/twilioClient.js
@@ -11,9 +11,10 @@ const client = twilio(accountSid, authToken);
 
 export const sendWhatsAppMessage = (to, message) => {
     client.messages.create({
+        contentSid: "HX37dd4c0f646c26f830c128c559099c1f",
+        messagingServiceSid: "MG17ccaeacb0652535dd6270d8e6397c6f",
         from: 'whatsapp:+13603429147', // El número de WhatsApp proporcionado por Twilio
-        to: `whatsapp:+505${to}`,
-        body: message,
+        to: `whatsapp:+505${to}`
     })
         .then(message => console.log('Mensaje enviado:', message.sid))
         .catch(err => console.error('Error al enviar mensaje:', err));
